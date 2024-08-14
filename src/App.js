8585bfdcf317acf92a175f8b701fefdccce3b59a
BishalMrzn <hapiebishal5@gmail.com>
2024-08-14 20:46:42 +0545

    first react app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+import './App.css';
+let name="Bishal";
+
+function App() {
+  return (
+    <>
+  <nav>
+    <li>Home</li>
+    <li>Menu</li>
+    <li>About Us</li>
+    <li>Contacts</li>
+  </nav>
+  <h1 className="title">The King of World is {name}</h1>
+  <div className="container">
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui quia exercitationem in expedita cum labore explicabo repudiandae! Aspernatur a commodi ducimus minus, accusantium rerum consequuntur voluptatum neque, sed voluptate fuga.</p></div></>
+  );
+}
+
+export default App;
